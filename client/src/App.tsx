0acd0303318dc3fbc61961eec1b6b4c007be2e5e
Daniel <56930814+w1qdev@users.noreply.github.com>
2024-08-07 23:48:20 +0500

    add AuthPage.tsx, LogIn + SignUp Components

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import AuthPage from "./pages/auth-page/AuthPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <AuthPage />
+    </div>
+  );
+}
+
+export default App;
